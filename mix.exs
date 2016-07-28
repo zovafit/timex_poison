9ddaaa96300e63a2ab888e1929100eb656d1f48f
Andrew Harvey <andrew@mootpointer.com>
2016-07-28 15:26:28 +1000

    Update mix.exs for publishing 0.1.0

--- mix.exs
+++ mix.exs
@@ -8,7 +8,9 @@ defmodule TimexPoison.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      consolidate_protocols: Mix.env != :test,
-     deps: deps()]
+     deps: deps(),
+     package: package()
+    ]
   end
 
   # Configuration for the OTP application
@@ -16,6 +18,23 @@ defmodule TimexPoison.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :timex]]
+  end
+
+  defp package do
+    [
+      licenses: ["BSD"],
+      maintainers: ["Andrew Harvey"],
+      links: %{
+        "GitHub" => "https://github.com/zovafit/timex_poison"
+      },
+      description: description()
+    ]
+  end
+
+  defp description do
+    """
+    Easily parse timestamps using Timex as they are decoded from json by Poison.
+    """
   end
 
   # Dependencies can be Hex packages:
@@ -31,7 +50,6 @@ defmodule TimexPoison.Mixfile do
     [
       {:timex, "~> 2.2"},
       {:poison, "~> 2.0"},
-      {:mex, "~> 0.0.1", only: :dev}
     ]
   end
 end
